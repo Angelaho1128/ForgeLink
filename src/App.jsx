ff3a822d7fcb326278733301dfe7008b78d84600
Angelaho1128 <hocheukwing1128@gmail.com>
2025-08-24 02:56:54 -0400

    updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,16 @@ import Chat from "./pages/Chat.jsx";
 import EnterInfo from "./pages/EnterInfo.jsx";
 import Login from "./pages/Login.jsx";
 import SignUp from "./pages/SignUp.jsx";
-import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import LoginPage from "./pages/Login.jsx";
-import App_test from "./App_test.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/test" element={<App_test />} />
-      </Routes>
-    </BrowserRouter>
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
-      <Route path="/enter-info" element={<EnterInfo />} />
       <Route path="/chat" element={<Chat />} />
+      <Route path="/enter-info" element={<EnterInfo />} />
     </Routes>
   );
 }
 
 export default App;
-
